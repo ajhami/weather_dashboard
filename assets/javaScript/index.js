243e857010811969b554803577577c111f2f8da7
Alex Hamilton <alexhami916@gmail.com>
2020-04-05 14:53:54 -0600

    Updated functions, fixed search history, and connected owm API.

--- assets/javaScript/index.js
+++ assets/javaScript/index.js
@@ -2,37 +2,127 @@
 /// index.js ///
 ////////////////
 
-var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
 
-console.log("index.js connected!");
-
-
-// Function and Event Listener to store the new city searched
-var saveCity = function(event){
+$(document).ready(function() {
     
-    cityList = JSON.parse(localStorage.getItem("cityList")) || [];
-
-    event.preventDefault();
     
-    if($("#searched_city").val() === "") {
-        console.log("Empty Search!");
-        return;
+    var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+    var owmAPIkey = "c0c4b29098bdb76042394a260bb65fc1";
+    var searchCity;
+    
+    console.log("index.js connected!");
+
+
+
+
+    // Render City List on function called and load
+    var renderCityList = function() {
+
+        $("#city_list").empty();
+
+        var cityListStored = JSON.parse(localStorage.getItem("cityList")) || [];
+    
+        
+        if(cityListStored.length === 0) {
+            console.log("No cities currently selected.");
+        }
+        
+    
+        for(var i = 0; i < cityListStored.length; i++) {
+    
+            var city = cityListStored[i];
+            var newLI = $("<li></li>").text(city);
+            $(newLI).appendTo("#city_list");
+    
+        }
+    }
+    
+    renderCityList();
+    
+    $("form").on("submit", function(event) {
+        event.preventDefault();
+        searchedCity = $("#searched_city").val().trim();
+        searchCity();
+    });
+    
+    // Function and Event Listener to store the new city searched
+    var searchCity = function() {
+        
+        cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+    
+        //event.preventDefault();
+        
+        if($("#searched_city").val() === "") {
+            console.log("Empty Search!");
+            return;
+        }
+    
+        console.log($("#searched_city").val());
+    
+        //var searchedCity = $("#searched_city").val().trim();
+        //searchedCity = $("#searched_city").val().trim();
+    
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + owmAPIkey;
+
+        console.log("Query URL = " + queryURL);
+
+        // WORK ON THIS NEXT AND ADD FUNCTIONALITY TO THE SEARCHWEATHERDATA FUNCTION
+
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            error: function(err) {
+                console.log(err.status + " " + err.statusText);
+            }
+        }).then(searchWeatherData);
+    
+    };
+    
+    /*
+    $("form").on("submit", function(event) {
+        event.preventDefault();
+        searchedCity = $("#searched_city").val().trim();
+        searchCity();
+    }
+    
+    );
+    */
+    
+    var searchWeatherData = function(owmData) {
+
+        console.log(owmData);
+        console.log("city name = " + owmData.name);
+        searchedCity = owmData.name;
+
+
+
+        saveCity();
+
+    };
+
+
+
+    var saveCity = function() {
+
+        cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+
+    
+        //var searchedCity = $("#searched_city").val().trim();
+
+        cityList.unshift(searchedCity);
+        cityList.splice(7);
+    
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+    
+        renderCityList();
     }
 
-    console.log($("#searched_city").val());
-
-    var newCity = $("#searched_city").val();
-
-    searchWeatherData(newCity);
-
-};
-
-$("form").on("submit", saveCity);
 
 
-var searchWeatherData = function(cityName) {
-    console.log(cityName + " weather data has been searched.");
-};
+
+
+});
+
 
 
 
